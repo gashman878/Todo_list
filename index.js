7db2cefb37180f0ec08dd9cb9ade8fa83e17d7c2
gashman878 <gashman878@gmail.com>
2025-05-29 07:20:28 +0100

    third

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ function addTodo() {
 
     inputed_todo.value = "";
 }
+
+
+const secondTo_doList = [];
+
+function secondAddTodo() {
+    const SecondInputed_todo = document.querySelector('.second_todo');
+    const SecondName = SecondInputed_todo.value;
+    
+    secondTo_doList.push(SecondName);
+    console.log(secondTo_doList);
+
+    for (let i = 0; i < secondTo_doList.length; i++) {
+        const element = secondTo_doList[i];
+        const html = "<p></p>"
+        
+    }
+
+
+
+
+    SecondInputed_todo.value = "";
+}
