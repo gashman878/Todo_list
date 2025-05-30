382e85b5f7a7d194ac4e74eefef27e9041ebfba9
gashman878 <gashman878@gmail.com>
2025-05-30 08:56:35 +0100

    eight

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const Todo_list = [];
-
-function addTodo() {
-    const inputed_todo = document.querySelector('.todo_input');
-    const name = inputed_todo.value;
-    
-    Todo_list.push(name);
-    console.log(Todo_list);
-
-    inputed_todo.value = "";
-}
-
-
-
-
-
 
 // Na here the second one start from
 
